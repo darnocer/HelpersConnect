18ad12afc3dc04aa4bd2fc8e7fa889c5c1d9a7ec
darnocer <darnocer@gmail.com>
2020-08-27 21:00:59 -0600

    merge conflicts

--- src/components/EventDetails/index.js
+++ src/components/EventDetails/index.js
@@ -4,7 +4,6 @@ import "./style.css";
 
 function EventDetails(props) {
   return (
-<<<<<<< HEAD
     <>
       <div className="col-8">
         <div className="event-details">
@@ -30,34 +29,6 @@ function EventDetails(props) {
           </ul>
         </div>
         <EventDescription />
-=======
-    <div className="col-8">
-      <div className="event-details">
-        <h2 className="text-uppercase is-size-6">
-          <strong>
-  <span className="event-name">{props.summary}</span>
-          </strong>
-        </h2>
-        <ul className="list-inline">
-          <li className="list-inline-item">
-            <i className="far fa-calendar-alt"></i>{" "}
-            <span class="day-of-week">Monday</span>
-          </li>
-          <li className="list-inline-item">
-            <i className="far fa-clock"></i>{" "}
-            <span className="start-time">6:00PM</span> -{" "}
-            <span className="end-time">8:00PM</span>
-          </li>
-          <li className="list-inline-item">
-            <i className="fas fa-map-marker-alt"></i>{" "}
-            <span className="location">Location</span>
-          </li>
-        </ul>
-        <p className="description">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        </p>
->>>>>>> dc3a15d42acad625cd7663fc3af6326490e4e5bc
       </div>
     </>
   );
