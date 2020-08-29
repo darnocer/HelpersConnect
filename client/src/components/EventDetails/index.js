6eea205a75e964ced2c8cc1fe8e4c0e00ea988c2
Katie Thorpe <kthorpe1023@gmail.com>
2020-08-29 13:55:01 -0600

    moment.js added to date and time formatting

--- client/src/components/EventDetails/index.js
+++ client/src/components/EventDetails/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import EventDescription from "../EventDescription";
 import "./style.css";
+import moment from 'moment'
 
 function EventDetails(props) {
   return (
@@ -15,12 +16,12 @@ function EventDetails(props) {
           <ul className="list-inline">
             <li className="list-inline-item">
               <i className="far fa-calendar-alt"></i>{" "}
-              <span className="day-of-week">Monday</span>
+  <span className="day-of-week">{moment(props.start).format('dddd')}</span>
             </li>
             <li className="list-inline-item">
               <i className="far fa-clock"></i>{" "}
-              <span className="start-time">{props.start}</span> -{" "}
-              <span className="end-time">{props.end}</span>
+              <span className="start-time">{moment(props.start).format('LT')}</span> -{" "}
+              <span className="end-time">{moment(props.end).format('LT')}</span>
             </li>
             <li className="list-inline-item">
               <i className="fas fa-map-marker-alt"></i>{" "}
