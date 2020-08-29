f40489da454d3204d85aefabd53bbef088380e68
darnocer <darnocer@gmail.com>
2020-08-29 14:02:32 -0600

    event card styled components

--- client/src/components/EventCard/index.js
+++ client/src/components/EventCard/index.js
@@ -1,21 +1,44 @@
 import React from "react";
 import EventData from "../EventData";
-import "./style.css";
+// import "./style.css";
+import styled from "styled-components";
+
+const accent = "var(--accent-color);";
+const primary = "var(--primary-color);";
+
+const Striped = styled.div`
+  .striped {
+    background-color: #ffffff;
+    border-left: 5px solid;
+    border-left-color: ${accent};
+    padding: 15px 0;
+  }
+
+  .striped:hover {
+    background-color: #ffffff;
+    border-left: 5px solid;
+    border-left-color: ${primary};
+    padding: 15px 0;
+  }
+`;
 
 function EventCard(props) {
   return (
-    <div className="columns is-centered">
-      <div className="column is-two-thirds">
-        <div className="card p-4 row-striped event-card">
-          <EventData 
-          title = {props.title}
-          start = {props.start}
-          end = {props.end}
-          description = {props.description}
-          location = {props.location}/>
+    <Striped>
+      <div className="columns is-centered">
+        <div className="column is-two-thirds">
+          <div className="card p-4 striped event-card">
+            <EventData
+              title={props.title}
+              start={props.start}
+              end={props.end}
+              description={props.description}
+              location={props.location}
+            />
+          </div>
         </div>
       </div>
-    </div>
+    </Striped>
   );
 }
 export default EventCard;
