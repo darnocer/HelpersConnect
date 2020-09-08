48fbb6d95b37e5cc690d8a137c8b49f468a1873f
darnocer <darnocer@gmail.com>
2020-09-08 17:48:22 -0600

    grabbing userID from auth

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Calendar from "./pages/Calendar";
 import Profile from "./pages/Profile";
+import Home from "./pages/Home";
 import NotFound from "./pages/Error";
 import Navbar from "./components/Navbar";
 import Wrapper from "./components/Wrapper";
@@ -11,9 +12,10 @@ function App() {
     <Router>
       <Wrapper>
         <Switch>
-          <Route exact path="/" component={Calendar} />
+          <Route exact path="/" component={Home} />
           <Route exact path="/profile" component={Profile} />
           <Route exact path="/calendar" component={Calendar} />
+          <Route exact path="*" component={Calendar} />
           <Route component={NotFound} />
         </Switch>
       </Wrapper>
