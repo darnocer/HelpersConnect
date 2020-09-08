1b0c86727eda476b13741627c4c59a68a2b9cabe
darnocer <darnocer@gmail.com>
2020-09-08 14:57:30 -0600

    basic styling for user profile page

--- client/src/components/UserData/index.js
+++ client/src/components/UserData/index.js
@@ -1,10 +1,15 @@
 import React from "react";
-import { Row, Col, Link } from "reactstrap";
+import { Row, Col } from "reactstrap";
 import styled from "styled-components";
 
-const Image = styled.div`
-  img {
-    border-radius: 50%;
+const Typography = styled.div`
+  a {
+    color: var(--primary-color);
+  }
+
+  a:hover {
+    color: var(--accent-color);
+    text-decoration: none;
   }
 `;
 
@@ -14,17 +19,34 @@ function UserData() {
       <div className="column is-two-thirds">
         <div className="card p-4">
           <Row>
-            <Image>
-              <Col>
-                <img src="https://via.placeholder.com/150" />
-              </Col>
-            </Image>
-            <Col>
-              <h2>
-                Welcome, <span id="userEmail">darnocer@gmail.com</span>!
-              </h2>
-              <a href="#">Sign Out</a>
+            <Col xs={3}>
+              <img
+                src="https://via.placeholder.com/150"
+                alt="user profile pic"
+                className="img-fluid rounded-circle"
+              />
             </Col>
+            <Typography>
+              <Col>
+                <h2>
+                  Welcome,{" "}
+                  <b>
+                    <span id="userEmail">email@email.com</span>
+                  </b>
+                  !
+                </h2>
+                <h3>
+                  You are attending{" "}
+                  <b>
+                    <span id="eventCount">X</span>
+                  </b>{" "}
+                  events.
+                </h3>
+                <br></br>
+                <br></br>
+                <a href="#">Sign Out</a>
+              </Col>
+            </Typography>
           </Row>
         </div>
       </div>
