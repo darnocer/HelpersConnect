110ef8b57421c67a0ca0dfeb6c47eae1149166cc
darnocer <darnocer@gmail.com>
2020-09-08 14:28:40 -0600

    conditionally render accept vs. decline

--- client/src/components/ResponseBtns/index.js
+++ client/src/components/ResponseBtns/index.js
@@ -1,17 +1,45 @@
 import React from "react";
-import "./style.css";
 
-function ResponseBtns() {
+import styled from "styled-components";
+
+const Buttons = styled.div`
+  @media only screen and (min-width: 768px) {
+    .button {
+      font-size: 1.5rem;
+    }
+  }
+
+  .accept i {
+    color: var(--primary-color);
+  }
+
+  .decline i {
+    color: var(--accent-color);
+  }
+
+  .button {
+    background-color: #ffffff;
+    border-radius: 0.5rem;
+    box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14),
+      0 3px 1px -2px rgba(0, 0, 0, 0.12), 0 1px 5px 0 rgba(0, 0, 0, 0.2);
+  }
+`;
+
+function ResponseBtns(props) {
   return (
     <div className="col-2">
-      <div className="has-text-centered">
-        <button className="button is-large mb-2 mb-md-0 accept">
-          <i className="far fa-calendar-check"></i>
-        </button>
-        <br />
-        <button className="button is-large decline">
-          <i className="far fa-calendar-times"></i>
-        </button>
+      <div className="has-text-centered mt-4">
+        <Buttons>
+          {props.accepted ? (
+            <button className="button decline">
+              <i className="far fa-calendar-times"></i>
+            </button>
+          ) : (
+            <button className="button accept">
+              <i className="far fa-calendar-check"></i>
+            </button>
+          )}
+        </Buttons>
       </div>
     </div>
   );
