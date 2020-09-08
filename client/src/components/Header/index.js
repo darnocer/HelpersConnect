00397e94c274969c15b25304d6d2891166e9f0d5
darnocer <darnocer@gmail.com>
2020-09-08 14:31:19 -0600

    header to styled components

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,12 +1,31 @@
 import React from "react";
-import "./style.css";
+
+import styled from "styled-components";
+
+const Heading = styled.header`
+  /* hide hr border on screens larger than mobile  */
+  @media only screen and (min-width: 768px) {
+    hr {
+      display: none;
+    }
+  }
+
+  /* line under header  */
+  hr {
+    border-top: 2px solid var(--accent-color);
+  }
+`;
 
 function Header() {
   return (
-    <header className="my-4">
-      <h1 className="title is-1 has-text-centered">Volunteer Calendar</h1>
-      <hr />
-    </header>
+    <Heading>
+      <header className="my-4">
+        <h1 className="title is-1 has-text-centered">
+          Volunteer Calendar <i class="fas fa-hands-helping"></i>
+        </h1>
+        <hr />
+      </header>
+    </Heading>
   );
 }
 
