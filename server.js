5a99818474cfdc8a9dcf2d173547ec4fb6df77c8
darnocer <darnocer@gmail.com>
2020-09-08 14:57:14 -0600

    resolving conflicts for server.js merging

--- server.js
+++ server.js
@@ -1,16 +1,7 @@
 const express = require("express");
-<<<<<<< HEAD
-
-//passport
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-=======
 const cookieSession = require("cookie-session");
->>>>>>> 6c3912f1b679bcd3fe2be09102f7554fc669f292
 const passport = require("passport");
 const mongoose = require("mongoose");
-<<<<<<< HEAD
-const routes = require("./routes/events");
-=======
 const bodyParser = require("body-parser");
 const routes = require("./routes/events");
 const keys = require("./config/keys");
@@ -18,7 +9,6 @@ const keys = require("./config/keys");
 require("./services/passport");
 
 const db = require("./models/index");
->>>>>>> 6c3912f1b679bcd3fe2be09102f7554fc669f292
 
 const app = express();
 
@@ -46,48 +36,6 @@ if (process.env.NODE_ENV === "production") {
 
 // Connect to the Mongo DB
 mongoose.connect(
-<<<<<<< HEAD
-  process.env.MONGODB_URI || "mongodb://localhost/volunteercalendar"
-);
-
-//authentication
-let GOOGLE_CLIENT_ID =
-  "262257493602-atfp32r9ptkbf7l29tnrb09dmi3es5am.apps.googleusercontent.com";
-let GOOGLE_CLIENT_SECRET = "ulMJ6xVCI5vEf4Drv0fkX2fG";
-
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: GOOGLE_CLIENT_ID,
-      clientSecret: GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3001/auth/google/callback",
-      scope: ["profile", "email"],
-    },
-    function (accessToken, refreshToken, profile, cb) {
-      console.log(profile);
-      //cb(null, profile.id);
-    }
-  )
-);
-
-app.get(
-  "/auth/google",
-  passport.authenticate("google", { scope: ["https://mail.google.com/"] })
-);
-
-app.get(
-  "/auth/google/callback",
-  passport.authenticate(
-    "google",
-    { successRedirect: "/auth/google" },
-    { failureRedirect: "/" }
-  ),
-  function (req, res) {
-    // Successful authentication, redirect home.
-    res.redirect("http://localhost:3000/");
-  }
-);
-=======
   process.env.MONGODB_URI || "mongodb://localhost/volunteercalendar",
   { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }
 );
@@ -96,7 +44,6 @@ require("./routes/authRoutes")(app);
 app.use(routes);
 
 const PORT = process.env.PORT || 3001;
->>>>>>> 6c3912f1b679bcd3fe2be09102f7554fc669f292
 
 app.listen(PORT, function () {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
